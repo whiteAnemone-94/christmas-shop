0cb838d72cd912a3b3eec976e703daf3d1b3771d
Olga Kamakhina <white.anemone.94@gmail.com>
2024-12-05 01:20:08 +0300

    fix: fix the work of the burger

--- christmas-shop/burger.js
+++ christmas-shop/burger.js
@@ -3,17 +3,54 @@ const navLinks = document.querySelectorAll('.nav__list-link');
 const navButton = document.querySelector('.burger-button');
 const overlay = document.querySelector('.overlay');
 
+// window.addEventListener('resize', () => {
+//   document.querySelector('body').setAttribute('style', 'overflow: visible');
+//   overlay.classList.remove('overlay_is-active');
+//   if (window.innerWidth > 768) {
+//     navButton.removeEventListener('click', (e) => {
+//       e.preventDefault();
+//       toggleMenu();
+//     });
+//     overlay.removeEventListener('click', () => toggleMenu());
+//     navLinks.forEach(el => {
+//       if (el.classList.contains('nav__list-link_active')) {
+//         el.setAttribute('style', 'pointer-events: none')
+//       }
+//       el.removeEventListener('click', () => toggleMenu())
+//     })
+//   } else if (window.innerWidth <= 768) {
+//     navButton.addEventListener('click', (e) => {
+//       e.preventDefault();
+//       toggleMenu();
+//     });
+//     overlay.addEventListener('click', () => toggleMenu());
+//     navLinks.forEach(el => {
+//       if (el.classList.contains('nav__list-link_active')) {
+//         el.setAttribute('style', 'pointer-events: all')
+//       }
+//       el.addEventListener('click', () => toggleMenu())
+//     })
+//   }
+// })
+
 // При клике на иконку бургера вызываем функцию toggleMenu
-navButton.addEventListener('click', (e) => {
-  e.preventDefault();
-  toggleMenu();
-});
+  navButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    toggleMenu();
+  });
 
-// При клике на ссылки вызываем функцию toggleMenu
-navLinks.forEach(el => el.addEventListener('click', () => toggleMenu()));
+if (window.innerWidth <= 768) {
+  // При клике на ссылки вызываем функцию toggleMenu
+  navLinks.forEach(el => {
+    if (el.classList.contains('nav__list-link_active')) {
+      el.setAttribute('style', 'pointer-events: all');
+    }
+    el.addEventListener('click', () => toggleMenu());
+  });
 
-// При клике на область вне меню вызываем функцию toggleMenu
-overlay.addEventListener('click', () => toggleMenu());
+  // При клике на область вне меню вызываем функцию toggleMenu
+  overlay.addEventListener('click', () => toggleMenu());
+}
 
 function toggleMenu() {
   // Переключаем стили элементов при клике: появляется меню, бургер превращается в крестик
